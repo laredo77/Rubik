11c119df727c8a30f1dfd99442cece877b7d9b51
laredo77 <Laredo93i@gmail.com>
2023-01-17 22:26:59 +0200

    stopwatch

---
+++ client/src/components/StopWatch/StopWatch.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useStopwatch } from 'react-timer-hook';
+
+function StopWatch() {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true });
+
+
+  return (
+    <div style={{textAlign: 'center'}}>
+      <div style={{fontSize: '50px'}}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+      {/* <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button> */}
+    </div>
+  );
+}
+
+export default StopWatch;
