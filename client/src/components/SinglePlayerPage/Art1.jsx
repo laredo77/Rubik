309422731d3e6f4bc1e029229f0e388c99544573
laredo77 <Laredo93i@gmail.com>
2023-03-06 11:17:44 +0200

    levels page

---
+++ client/src/components/SinglePlayerPage/Art1.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { cubesImage } from "../cubesImage";
+import "./Art1.css";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import Button from "@mui/material/Button";
+
+const theme = createTheme();
+
+function Art1() {
+  const MySwal = withReactContent(Swal);
+  //const generateImage = (response) => {};
+  const handleImageClick = (response) => {
+    MySwal.fire({
+      title: "Solver",
+      html: (
+        <i>
+          {/*{`hihihi`}*/}
+          {/*<br />*/}
+          <Button
+            variant="contained"
+            // sx={{ marginRight: 0, marginLeft: "auto", display: "block" }}
+          >
+            Upload cube images to solve
+          </Button>
+        </i>
+      ),
+      imageUrl: response.target.src,
+      imageHeight: 90,
+      confirmButtonColor: "#50b7f5",
+      showCloseButton: true,
+      showCancelButton: true,
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ImageList
+        sx={{
+          marginRight: "auto",
+          marginLeft: "auto",
+          marginTop: "auto",
+          marginBottom: "auto",
+          width: 693, // 990 * 0.7
+          height: 567, // 810 * 0.7
+        }}
+        cols={30}
+        //rowHeight={0}
+        gap={0.5}
+      >
+        {cubesImage.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={item.img}
+              alt={item.title}
+              loading="lazy"
+              className={"zoom"}
+              onClick={handleImageClick}
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </ThemeProvider>
+  );
+}
+
+export default Art1;
---
+++ client/src/components/cubesImage.js
@@ -0,0 +1,8 @@
+export const cubesImage = [];
+for (let i = 0; i < 900; i++) {
+  let new_im = {
+    label: i.toString(),
+    img: "/final-cubes/" + i.toString() + ".png",
+  };
+  cubesImage.push(new_im);
+}
