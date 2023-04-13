40dd23fc67de7292f60f55f7086c50f715aa1066
laredo77 <Laredo93i@gmail.com>
2023-04-13 10:52:09 +0300

    CompMode: routes

--- client/src/components/ComponentsManager.jsx
+++ client/src/components/ComponentsManager.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import MainPageConnector from "./MainPage/MainPageConnector";
 import FreePlayPage from "./FreeGame/FreePlayPage";
 import Art1Connector from "./Mosaic/LevelsPage/GameLevels/Art1Connector";
@@ -14,28 +14,28 @@ import MatchManagerConnector from "./Match/MatchManagerConnector";
 import MatchPageConnector from "./Match/MatchPageConnector";
 
 function ComponentsManager() {
-  return (
-    <div className="main">
-      <Routes>
-        <Route path="/" element={<LoginPageConnector />} />
-        <Route path="/main" element={<MainPageConnector />} />
-        <Route path="/main/game/freePlay" element={<FreePlayPage />} />
-        <Route path="/main/gameLevels/3" element={<Art1Connector />} />
-        <Route path="/main/game/mosaic/levels" element={<LevelsPageConnector />} />
-        <Route path="/main/matchManager" element={<MatchManagerConnector />} />
-        <Route path="/main/matchManager/match" element={<MatchPageConnector />} />
-        <Route
-          path="/main/singlePlayerCompPage"
-          element={<CompetitionModeConnector />}
-        />
-        <Route path="/main/learn" element={<LearnPage/>}/>
-        <Route path="/main/learn/basics" element={<BasicsPage/>}/>
-        <Route path="/main/learn/algorithms" element={<AlgorithmsPage/>}/>
-        <Route
-          path="/main/singlePlayerCompPage/leaderBoard"
-          element={<LeaderBoardPage />}
-        />
-        <Route path="/main/game/mosaic" element={<MosaicConnector />} />
+    return (
+        <div className="main">
+            <Routes>
+                <Route path="/" element={<LoginPageConnector/>}/>
+                <Route path="/main" element={<MainPageConnector/>}/>
+                <Route path="/main/game/freePlay" element={<FreePlayPage/>}/>
+                <Route path="/main/gameLevels/3" element={<Art1Connector/>}/>
+                <Route path="/main/game/mosaic/levels" element={<LevelsPageConnector/>}/>
+                <Route path="/main/matchManager" element={<MatchManagerConnector/>}/>
+                <Route path="/main/matchManager/match" element={<MatchPageConnector/>}/>
+                <Route
+                    path="/main/game/competition"
+                    element={<CompetitionModeConnector/>}
+                />
+                <Route path="/main/learn" element={<LearnPage/>}/>
+                <Route path="/main/learn/basics" element={<BasicsPage/>}/>
+                <Route path="/main/learn/algorithms" element={<AlgorithmsPage/>}/>
+                <Route
+                    path="/main/singlePlayerCompPage/leaderBoard"
+                    element={<LeaderBoardPage/>}
+                />
+                <Route path="/main/game/mosaic" element={<MosaicConnector/>}/>
             </Routes>
         </div>
     );
